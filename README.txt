2a0c6aa1541d8a0c45c2bc71bbd2204e83c63678
riroraku <riroraku69.ph@gmail.com>
2026-03-01 23:55:03 +0800

    Initial

---
+++ README.txt
@@ -0,0 +1,50 @@
+NOTES ON PROGRAM USE:
+DO NOT USE THIS PROGRAM IN PRODUCTION! IT IS ONLY INTENDED 
+FOR PERSONAL USE AND FOR CLARIFICATION I DID TEST ONLY ON
+TERMUX (NO ROOT). IF YOU TRY TO USE, TO RUN SCRIPT FROM FULL
+FLEDGE SYSTEM THIS PROGRAM MIGHT NOT RUN IN SOME CASES.
+WHEN I SAY PERSONAL, I MEAN YOUR PERSONAL PROJECTS OR YOUR
+PERSONAL USER IN THE SYSTEM (RUN ONLY YOUR OWNED SCRIPTS).
+
+THIS PROGRAM USES system() FUNCTION! PLEASE BE NOTED!
+
+DESCRIPTION
+This is a small task scheduler intended to run scripts on loop! 
+This program consists of (2) args:
+		scheduler [minute] [script_file]
+
+		[minute]       Run script file  for every 1 - 1440 minutes.
+		[script_file]  A script file to run.
+
+To run program you can either run it on background with (&)
+		(e.g.) ./scheduler 60 log_users &
+or foreground
+		(e.g.) ./scheduler 60 log_users
+
+COMPILATION
+Just compile it normally!
+		gcc scheduler.c -o scheduler
+then run!
+
+TEST ENVIRONMENT/MACHINE
+Linux 5.10.209-android12-9-00016-g7c6bbcca33e1-ab12029497 #1
+SMP PREEMPT Fri Jun 28 13:58:53 UTC 2024 aarch64 Android 13
+
+TERMUX VERSION
+0.118.3 (non rooted)
+
+GCC 
+clang version 21.1.8
+Target: aarch64-unknown-linux-android24
+Thread model: posix
+InstalledDir: /data/data/com.termux/files/usr/bin
+
+Q&A
+Q. Why using system() for beep character?
+A. I tried using printf() for that character it seems doesnt
+work so no choice i used system() calling echo -ne "\a".
+
+NOT RELATED
+I program this one on phone with nano! Though theres a decent
+on mobile phones but it seem i really gotten hang-up on nano
+especially when i discovered nano could be configured!
---
+++ scheduler.c
@@ -0,0 +1,57 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+//#include <string.h>
+
+static void validate_n_parse_args(const char**, unsigned short*);
+
+int main(int args, char** argv) {
+	unsigned short s = 0;
+	char* cmd;
+	size_t cmd_len;
+
+	if(args == 3) {
+		validate_n_parse_args((const char**)argv, &s);
+		cmd_len = 10 + strlen(argv[2]);
+		if((cmd = malloc(cmd_len)) == NULL) perror("Unable to allocate memory!");
+		memset(cmd, 0, cmd_len); // to make buf has null byte for string
+		snprintf(cmd, cmd_len, "%s%s", "source ", argv[2]);
+		while(1) {
+			system(cmd);
+			system("echo -n \a"); // beeps per defined minute(s) (SEE README.txt for explanation!)
+			sleep(s * 60);
+		}
+	}
+	printf("Usage: %s [minutes] [script_file]\n", argv[0]);
+	return 1;
+}
+
+static void validate_n_parse_args(const char** argv, unsigned short* seconds) {
+	size_t i = 0;
+
+	// validate for minute arg
+	while(argv[1][i] != '\0') {
+		if(!(argv[1][i] >= '0' && argv[1][i] <= '9')) {
+			puts("Arg [minute] should be characters between (0 - 9)!");
+			exit(1);
+		}
+		// convert char 0 - 9 as short int
+		*seconds = *seconds * 10 + (argv[1][i] - '0');
+		i++;
+	}
+	if(!(*seconds >= 1 && *seconds <= 1440)) {
+		puts("Arg [minute] only supports 1 - 1440 minutes!");
+		exit(1);
+	}
+	// i = 0;
+	// vwlidate for script_file (checks if exist as well as
+	// permission for executable!
+	if(access(argv[2], F_OK) == -1) {
+		puts("Arg [script_file] doesn't exist! Make sure you entered the right path or should exists!");
+		exit(1);
+	}
+	if(access(argv[2], X_OK) == -1) {
+		puts("Arg [script_file] doesn't have executable permission!");
+		exit(1);
+	}
+}
