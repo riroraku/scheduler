cd4b45da0f26c50746f910cc80a458f06f6840f7
riroraku <riroraku69.ph@gmail.com>
2026-03-24 15:04:39 +0800

    Added short copyright notice

--- scheduler.c
+++ scheduler.c
@@ -1,3 +1,28 @@
+/*
+ *  scheduler - A simple script scheduler.
+ *
+ *    Copyright (C) 2026  riroraku
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License along
+ *    with this program; if not, write to the Free Software Foundation, Inc.,
+ *    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This General Public License does not permit incorporating your program into
+ * proprietary programs.  If your program is a subroutine library, you may
+ * consider it more useful to permit linking proprietary applications with the
+ * library.  If this is what you want to do, use the GNU Lesser General
+ * Public License instead of this License.
+ */
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
